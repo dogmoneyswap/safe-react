901e1d60388b3aba562f32f84d03bd3345a1efb8
mainnet-pat <mainnet_pat@protonmail.com>
2022-05-16 10:45:46 +0000

    Never fetch currency codes. Backend supports only usd anyway

--- src/logic/currencyValues/api/fetchAvailableCurrencies.ts
+++ src/logic/currencyValues/api/fetchAvailableCurrencies.ts
@@ -1,10 +1,7 @@
-import { FiatCurrencies, getFiatCurrencies } from '@gnosis.pm/safe-react-gateway-sdk'
-import { GATEWAY_URL } from 'src/utils/constants'
+import { FiatCurrencies /*, getFiatCurrencies*/ } from '@gnosis.pm/safe-react-gateway-sdk'
+// import { GATEWAY_URL } from 'src/utils/constants'
 
 export const fetchAvailableCurrencies = async (): Promise<FiatCurrencies> => {
-  try {
-    return getFiatCurrencies(GATEWAY_URL)
-  } catch {
-    return ['USD']
-  }
+  // return getFiatCurrencies(GATEWAY_URL)
+  return ['USD']
 }
